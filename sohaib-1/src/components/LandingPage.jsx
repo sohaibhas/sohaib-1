18612b5eb3bad8b778ed13f37a3a44608f1fb4a2
sohaibhas <90102630+sohaibhas@users.noreply.github.com>
2022-02-09 15:28:47 +0500

    4

--- sohaib-1/src/components/LandingPage.jsx
+++ sohaib-1/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './LandingPages/About';
 import Header from './LandingPages/Header';
 import Hero from './LandingPages/Hero';
 
@@ -6,6 +7,7 @@ const LandingPage = () => {
     return ( <div>
         <Header />
         <Hero />
+        <About />
     </div> );
 }
  
---
+++ sohaib-1/src/components/LandingPages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./About.css";
+
+const About = () => {
+    return ( <div className="about">
+        <div className="right">
+            <h1>ABOUT ME</h1>
+        </div>
+        <div className="left">
+            <h1>I'M A SOHAIB HASSAN</h1>
+        </div>
+    </div> );
+}
+ 
+export default About;
